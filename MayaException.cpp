1986ce4249e6759048c2bcf0a429f84519db35ff
Tim Milstead <tmilstead@datagraphic.co.uk>
2015-10-28 07:17:11 +0000

    Convert line number to string without relying on Boost libraries

--- MayaException.cpp
+++ MayaException.cpp
@@ -1,8 +1,15 @@
 //(c) Tim Milstead 2015
 
 #include "MayaException.h"
-#include <boost/lexical_cast.hpp>
 
 MayaException::MayaException(const MStatus& status, const char* file, int line):
-	std::runtime_error(std::string(status.errorString().asChar())+std::string(". ")+file+std::string(", line ")+boost::lexical_cast<std::string>(line), status(status)
+	std::runtime_error
+	(
+		std::string(status.errorString().asChar())+
+		std::string(". ")+
+		file+
+		std::string(", line ")+
+		static_cast<std::ostringstream*>( &(std::ostringstream() << line) )->str()
+	),
+	status(status)
 {}
--- MayaException.h
+++ MayaException.h
@@ -3,11 +3,18 @@
 #ifndef MAYA_EXCEPTION_H
 #define MAYA_EXCEPTION_H
 
+// -------- Language Headers --------
+
 #include <stdexcept>
 #include <string>
+#include <sstream>
+
+// -------- Maya Headers --------
 
 #include <maya/MStatus.h>
 #include <maya/MString.h>
+
+// -------- Macros --------
 
 // Requires declared MStatus status
 #define ASSERT_PASSED_MSTATUS(CODE) CODE; if (status != MStatus::kSuccess) {throw MayaException(status, __FILE__, __LINE__);}
@@ -15,6 +22,8 @@
 // Self contained MStatus
 // Warning: Internal declarations will be scoped to this block (unlikely to be an issue)
 #define ASSERT_RETURN_MSTATUS(CODE) { MStatus assertReturnMStatus = CODE; if (assertReturnMStatus != MStatus::kSuccess) {throw MayaException(assertReturnMStatus, __FILE__, __LINE__);} }
+
+// -------- Class --------
 
 class MayaException : public std::runtime_error
 {
